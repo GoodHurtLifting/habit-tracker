2e6f13995f28c24717a19a91372e995aa40e9d4a
GoodHurtLifting <ryan@theliftleague.com>
2026-03-21 18:30:28 -0400

    Polish weekly summary card on home screen

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -2,6 +2,7 @@ import 'package:intl/intl.dart';
 
 class DateFormatter {
   static final DateFormat _weekdayMonthDayFormat = DateFormat('EEE, MMM d');
+  static final DateFormat _monthDayFormat = DateFormat('MMM d');
 
   static String weekdayMonthDay(DateTime date) {
     return _weekdayMonthDayFormat.format(date);
@@ -9,5 +10,13 @@ class DateFormatter {
 
   static DateTime normalize(DateTime date) {
     return DateTime(date.year, date.month, date.day);
+  }
+
+  static String monthDay(DateTime date) {
+    return _monthDayFormat.format(date);
+  }
+
+  static String weekRange(DateTime start, DateTime end) {
+    return '${monthDay(start)} – ${monthDay(end)}';
   }
 }
